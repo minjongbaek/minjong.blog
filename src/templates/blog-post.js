2ca72b4f3a43cd3fdc90c746c505d6c6bfa264db
minjongbaek <minjongbaek@gmail.com>
2022-01-30 17:07:27 +0900

    style: 날짜 포맷 수정, 코드블록 폰트 크기 수정

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -87,7 +87,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "YYYY-MM-DD")
         description
       }
     }
