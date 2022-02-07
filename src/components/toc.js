eb2e610ece203878c00be1469b04606076060caf
minjongbaek <minjongbaek@gmail.com>
2022-02-07 18:51:07 +0900

    fix: 목차 스타일 적용 안되는 이슈 수정

--- src/components/toc.js
+++ src/components/toc.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Toc = ({ toc }) => {
-  return <div className='toc' dangerouslySetInnerHTML={{ __html: toc }}></div >;
+  return <div className='toc-wrap'>
+    <div className='toc' dangerouslySetInnerHTML={{ __html: toc }}></div >
+  </div>
+    ;
 };
 
 export default Toc;
