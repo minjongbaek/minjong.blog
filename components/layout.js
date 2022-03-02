2cf2c591c5e5066ad6752aadcd73fd246a8b562c
minjongbaek <minjongbaek@gmail.com>
2022-03-02 23:30:52 +0900

    refector: change framework
    - Gatsby -> Next.js
    - header 작성

---
+++ components/layout.js
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import styles from './layout.module.css'
+
+const name = '백민종'
+export const siteTitle = '민종로그'
+
+const Layout = ({ children, home }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Learn how to build a personal website using Next.js" />
+        <meta
+          property="og:image"
+          content="https://og-image.vercel.app/Minjong%20Log.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg"
+        />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+
+      <header className={styles.header}>
+        <Image
+          priority
+          src="/images/profile.png"
+          className={styles.profileImage}
+          height={100}
+          width={100}
+          alt={name}
+        />
+        <p className={styles.profileSumary}>
+          <strong>{name}</strong> <br />
+          2년차 주니어 개발자입니다.<br />
+          프론트 엔드 기술에 관심이 많습니다.
+        </p>
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Layout
