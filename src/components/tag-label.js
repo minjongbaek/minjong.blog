40cdad5731b2716cac65ea106a1118c9da00e36f
minjongbaek <minjongbaek@gmail.com>
2022-07-04 16:24:18 +0900

    fix: JavaScript 태그 페이지로 접근이 안되는 이슈

--- src/components/tag-label.js
+++ src/components/tag-label.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from "gatsby"
 
+import kebabCase from "lodash/kebabCase"
+
 const TagLabel = ({ tag }) => {
   return (
     <span className="tag-label">
-      <Link to={`/tags/${tag.toLowerCase()}`}>{tag}</Link>
+      <Link to={`/tags/${kebabCase(tag)}`}>{tag}</Link>
     </span>
   )
 }
